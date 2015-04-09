f993f03cfb578e37404037e4a4d5314402938be9
bmongkol <mboonwong@gmail.com>
2015-04-09 19:11:32 -0400

    transitionprove

--- work.js
+++ work.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
-//        console.log ("my js is loaded");
-        $('#tribeca-loft img').mouseenter (function(){
-            console.log("my mouse entered this image")
-        });
-        $('tribeca-loft img') .mouseleave (function(){
-            console.log("my mouse left this image")
-        });
+$(document).ready(function () {
+    //        console.log ("my js is loaded");
+    $('#tribeca-loft img').mouseenter(function () {
+        console.log("my mouse entered this image")
+    });
+    $('tribeca-loft img').mouseleave(function () {
+        console.log("my mouse left this image")
+    });
 });
